135d2d27596d58b7a1d1c1d3055ae6bbe41e6f0e
faisalOmarBadaam <151966557+faisalOmarBadaam@users.noreply.github.com>
2025-10-04 13:41:34 +0300

    Create server.js

---
+++ config/env/production/server.js
@@ -0,0 +1,7 @@
+module.exports = ({ env }) => ({
+  url: env("URL", "https://www.api.bwiscompltd.com"),
+  proxy: true,
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
+  app: { keys: env.array("APP_KEYS") },
+});
